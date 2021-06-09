c1388e249da02c15d0e07b9dde1ef6bb4cbe5a8c
Reggie <reggieduran15@gmail.com>
2021-06-09 10:29:35 +0800

    Changed some functions into arrow functions

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
-const getPlayerChoice = function() {
+const getPlayerChoice = () => {
     const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
     if (
         selection !== ROCK && 
@@ -23,7 +23,7 @@ const getPlayerChoice = function() {
     return selection;
 };
 
-const getComputerChoice = function() {
+const getComputerChoice = () => {
     const randomValue = Math.random();
     if (randomValue < 0.34) {
         return ROCK;
@@ -42,8 +42,8 @@ const getWinner = (cChoice, pChoice) =>
           (cChoice === SCISSORS && pChoice === ROCK)
         ? RESULT_PLAYER_WINS
         : RESULT_COMPUTER_WINS;
-        
-startGameBtn.addEventListener('click', function() {
+
+startGameBtn.addEventListener('click', () => {
     if (gameIsRunning) {
         return;
     }
